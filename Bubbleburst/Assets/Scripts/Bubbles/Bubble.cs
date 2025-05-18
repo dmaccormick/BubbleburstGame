447e6c5c3b1a59adbeec42a680a84b8dd893e254
Dan <dmaccormick@hotmail.com>
2025-05-18 05:15:12 +0200

    Setup a basic interaction system with mouse raycasting

--- Bubbleburst/Assets/Scripts/Bubbles/Bubble.cs
+++ Bubbleburst/Assets/Scripts/Bubbles/Bubble.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using DanMacC.BubbleBurst.Interactions;
+using UnityEngine;
 
 namespace DanMacC.BubbleBurst.Bubbles
 {
@@ -13,10 +14,28 @@ namespace DanMacC.BubbleBurst.Bubbles
         None
     }
 
-    public class Bubble : MonoBehaviour
+    public class Bubble : MonoBehaviour, IInteractable
     {
         public BubbleColour Colour => m_BubbleColour;
 
         [SerializeField] private BubbleColour m_BubbleColour;
+        [SerializeField] private Transform m_Visuals;
+
+        #region IInteractable
+        public void OnTargetingStart()
+        {
+            m_Visuals.localScale = Vector3.one * 1.5f;
+        }
+
+        public void OnTargetingEnd()
+        {
+            m_Visuals.localScale = Vector3.one;
+        }
+
+        public void OnClicked()
+        {
+            m_Visuals.localScale = Vector3.one * 2.0f;
+        }
+        #endregion IInteractable
     }
 }
